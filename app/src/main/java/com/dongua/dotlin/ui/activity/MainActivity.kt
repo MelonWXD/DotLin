07c86583c1b512453f87d7c46fcabd7f1301fa81
Lewis <a549295838@163.com>
2018-01-15 21:12:50 +0800

    调整MainUI

--- app/src/main/java/com/dongua/dotlin/ui/activity/MainActivity.kt
+++ app/src/main/java/com/dongua/dotlin/ui/activity/MainActivity.kt
@@ -14,6 +14,7 @@ import com.dongua.dotlin.ui.adapter.NoteAdapter
 import com.dongua.dotlin.ui.base.BaseActivity
 import com.dongua.dotlin.ui.fragment.NoteFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_recycler_drawer_left.*
 import javax.inject.Inject
 
 /**
