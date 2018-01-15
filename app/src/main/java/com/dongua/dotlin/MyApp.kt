ad06a5a5b0a9e9b83d5f87601188863da1de2b99
Lewis <a549295838@163.com>
2018-01-15 14:31:23 +0800

    logger引入

--- app/src/main/java/com/dongua/dotlin/MyApp.kt
+++ app/src/main/java/com/dongua/dotlin/MyApp.kt
@@ -4,6 +4,12 @@ import android.app.Application
 import com.dongua.dotlin.di.components.AppComponent
 import com.dongua.dotlin.di.components.DaggerAppComponent
 import com.dongua.dotlin.di.modules.AppModule
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger.addLogAdapter
+import com.orhanobut.logger.PrettyFormatStrategy
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.Logger
+
 
 //import com.dongua.dotlin.di.components.AppComponent
 //import com.dongua.dotlin.di.components.DaggerAppComponent
@@ -26,6 +32,18 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         appComponent = DaggerAppComponent.builder().appModule(AppModule(this)).build()
+
+        initLog()
+
+    }
+
+    private fun initLog() {
+        val formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(false)
+                .methodCount(1)
+                .tag("WXD")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+                .build()
+        Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
 
     }
 }
